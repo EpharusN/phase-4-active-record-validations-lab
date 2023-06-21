4b9767e3f4a8a641a33fa76eda0de6ae859099e9
Epharus Ndumia <epharusndumia@gmail.com>
2023-06-21 19:40:21 +0300

    final commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+#has_many :posts
+validates :name, presence: true
+validates :name, uniqueness: {case_sensitive: true}
+validates :phone_number, length: {is: 10}
 end
+
+#puts Author.create(name: "Mary Jane").valid?
+#puts Author.create(name: nil),valid?
